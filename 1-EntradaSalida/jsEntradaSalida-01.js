7ee02b53eb4f3f9da2b0fed43f2d75d57893ee67
velazquezmarcelo <marcelovelazquez994@gmail.com>
2022-11-15 18:01:40 -0300

    Alerta cambiada

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Mundo");
+	alert("Bienvenido, esta acción se ha modificado");
 }
 
